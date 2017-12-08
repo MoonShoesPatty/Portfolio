e944140c2eb817327f1a67b6f80a61d7d5b0827c
Pat Johnston <pattrashgarbage@gmail.com>
2017-12-08 14:46:56 -0500

    Added & styled about section

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -4,19 +4,34 @@ const portfolio = {};
 // New controller - used for ScrollMagic
 portfolio.controller = new ScrollMagic.Controller();
 
-// Create a new "scrollMagic scene"
+// Inner height of viewport - used to fade out landing page on scroll
+portfolio.windowHeight = window.innerHeight;
+
+// Create a new ScrollMagic scene
 portfolio.scrollMagicScene = function (elementID, classToAdd) {
-	var ourScene = new ScrollMagic.Scene({
+	let newScene = new ScrollMagic.Scene({
 		triggerElement: elementID
 	})
 	.setClassToggle(elementID, classToAdd)
 	.addTo(portfolio.controller)
 }
 
+// A new ScrollMagic scene with a duration
+portfolio.scrollMagicSceneDuration = function (elementID, classToAdd, customDuration) {
+	//const tween = new TweenMax.to(elementID, 1, {opacity: 0})
+	let newScene = new ScrollMagic.Scene({
+		triggerElement: elementID,
+		duration: customDuration
+	})
+		//.setTween(tween)
+		.addTo(portfolio.controller)
+}
+
 // Function to run on Document Ready
 portfolio.init = function () {
-	portfolio.scrollMagicScene('#portfolioItem01', 'portfolioFadeIn')
-	portfolio.scrollMagicScene('#portfolioItem02', 'portfolioFadeIn')
+	portfolio.scrollMagicScene('#portfolioItem01', 'portfolioFadeIn');
+	portfolio.scrollMagicScene('#portfolioItem02', 'portfolioFadeIn');
+	portfolio.scrollMagicSceneDuration('#landingPage', 'headerFadeOut', portfolio.windowHeight);
 }
 
 // Document Ready
