1d9d66f198f87ec402e1c499a0cc611a3379a715
Pat Johnston <pattrashgarbage@gmail.com>
2017-12-11 16:28:06 -0500

    Skills list & animations

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -5,12 +5,13 @@ const portfolio = {};
 portfolio.controller = new ScrollMagic.Controller();
 
 // Inner height of viewport - used to fade out landing page on scroll
-portfolio.windowHeight = window.innerHeight;
+portfolio.offset = window.innerHeight/-2.2;
 
 // Create a new ScrollMagic scene
 portfolio.scrollMagicScene = function (elementID, classToAdd) {
 	let newScene = new ScrollMagic.Scene({
-		triggerElement: elementID
+		triggerElement: elementID,
+		offset: portfolio.offset
 	})
 	.setClassToggle(elementID, classToAdd)
 	.addTo(portfolio.controller)
@@ -34,6 +35,25 @@ portfolio.smIconScene = function (elementClass, classToAdd) {
 		.addTo(portfolio.controller)
 }
 
+// Line on each section heading
+portfolio.headingLineScene = function (elementID, classToAdd) {
+	let newScene = new ScrollMagic.Scene({
+		triggerElement: elementID,
+		offset: portfolio.offset
+	})
+		.setClassToggle(elementID, classToAdd)
+		.addTo(portfolio.controller)
+}
+
+portfolio.skillsList = function (triggerID, elementClass, classToAdd) {
+	let newScene = new ScrollMagic.Scene({
+		triggerElement: triggerID,
+		offset: portfolio.offset
+	})
+		.setClassToggle('.skillItem', classToAdd)
+		.addTo(portfolio.controller)
+}
+
 // A new ScrollMagic scene with a duration
 portfolio.scrollMagicSceneDuration = function (elementID, classToAdd, customDuration) {
 	//const tween = new TweenMax.to(elementID, 1, {opacity: 0})
@@ -45,13 +65,25 @@ portfolio.scrollMagicSceneDuration = function (elementID, classToAdd, customDura
 		.addTo(portfolio.controller)
 }
 
-// Function to run on Document Ready
-portfolio.init = function () {
+portfolio.scrollMagicAnimations = function() {
+	//Portfolio Items
 	portfolio.scrollMagicScene('#portfolioItem01', 'portfolioFadeIn');
 	portfolio.scrollMagicScene('#portfolioItem02', 'portfolioFadeIn');
+	//Heading Line
+	portfolio.headingLineScene('#about__headerLine', 'sectionHeader__line--slide');
+	portfolio.headingLineScene('#portfolio__headerLine', 'sectionHeader__line--slide');
+	portfolio.headingLineScene('#contact__headerLine', 'sectionHeader__line--slide');
+	//Skills List
+	portfolio.skillsList('#skillsList', '.skillItem', 'skillItemSlide');
 	//portfolio.removeLandingPage('#FN', 'pageTitleFN');
+	//Social Media Icons
 	portfolio.smIconScene('.smIcon__icon', 'smIcon__icon--slideIn');
 	//portfolio.scrollMagicSceneDuration('#landingPage', 'headerFadeOut', portfolio.windowHeight);
+}
+
+// Function to run on Document Ready
+portfolio.init = function() {
+	portfolio.scrollMagicAnimations();
 }
 
 // Document Ready
