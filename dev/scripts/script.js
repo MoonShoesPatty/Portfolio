6c001e1c6ffb5a2bd9b0416a60873401e6c3b514
Pat Johnston <pattrashgarbage@gmail.com>
2017-12-21 15:31:01 -0500

    added ScrollMagic scene for js30 portfolio item

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -87,6 +87,7 @@ portfolio.scrollMagicAnimations = function() {
 	portfolio.portfolioItems('#portfolioReact', 'portfolioFadeIn');
 	portfolio.portfolioItems('#portfolioHearthstone', 'portfolioFadeIn');
 	portfolio.portfolioItems('#portfolioResponsive', 'portfolioFadeIn');
+	portfolio.portfolioItems('#portfolioJS30', 'portfolioFadeIn');
 	//Heading Line
 	portfolio.headingLineScene('#about__headerLine', 'sectionHeader__line--slide');
 	portfolio.headingLineScene('#portfolio__headerLine', 'sectionHeader__line--slide');
